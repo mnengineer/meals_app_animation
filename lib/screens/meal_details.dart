3210b477e08ab2e97d883195bd7c77c69d834f39
mnengineer <masashiii0115@gmail.com>
2023-08-12 14:07:13 +0900

    feat: アニメーションの時間変更

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -36,9 +36,15 @@ class MealDetailsScreen extends ConsumerWidget {
             icon: AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
-                return RotationTransition(turns: animation, child: child);
+                return RotationTransition(
+                  turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
               },
-              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           )
         ]),
